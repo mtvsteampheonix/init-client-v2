4350bfe8d6d8244da5b941396d2a57ba25d5f803
NJWonE <93060795+NJWonE@users.noreply.github.com>
2022-10-13 10:54:43 +0900

    Feat: "지원자 목록 조회" 추가
    기업회원이 특정 자신의 공고에 지원한
    지원자들을 목록으로 확인하는 페이지 추가.

--- src/routes/mypages/CompanyMyPage.js
+++ src/routes/mypages/CompanyMyPage.js
@@ -1,6 +1,12 @@
 import {Route} from 'react-router-dom';
 import Test from './../../pages/Test';
+import ApplyListCompany from './../../pages/match/ApplyListCompany';
 
 export default function CompanyMyPage() {
-  return <Route index element={<Test />}></Route>;
+  return (
+    <>
+      <Route path='apply-list' element={<ApplyListCompany />} />
+      <Route index element={<Test />}></Route>
+    </>
+  );
 }
