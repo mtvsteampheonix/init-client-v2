8c9ab4c0baf79e3b19abcfd04954c395c59dff3a
5onchangwoo <scw9410@gmail.com>
2022-10-13 10:56:04 +0900

    Feat: "기업회원 정보 수정" 추가

--- src/routes/mypages/CompanyMyPage.js
+++ src/routes/mypages/CompanyMyPage.js
@@ -1,6 +1,14 @@
 import {Route} from 'react-router-dom';
+import EditCompanyMemberInfo from '../../pages/members/mypages/EditCompanyMemberInfo';
 import Test from './../../pages/Test';
+import Withdraw from './../../pages/members/withdraws/Withdraw';
 
 export default function CompanyMyPage() {
-  return <Route index element={<Test />}></Route>;
+  return (
+    <>
+      <Route index element={<Test />}></Route>;
+      <Route path='edit-profile' element={<EditCompanyMemberInfo />} />
+      <Route path='withdraw' element={<Withdraw />} />
+    </>
+  );
 }
